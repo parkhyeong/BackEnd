8c589abeaf842b4efa33e6eb2f79927bc634133b
juhyeong <ghd5427@naver.com>
2023-09-21 15:21:50 +0900

    새 프로젝트 2

---
+++ javabasic/src/javabasic/Hi.java
@@ -0,0 +1,13 @@
+package javabasic;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕하세요.!1 드디어실행!!");
+		System.out.println("안녕!! 쉬는시간이 다가와요.!!");
+		//syso 
+		System.out.println();
+	}
+
+}
